61d3a4f8d7cb748b3bca9f557db1867804a6d83e
Hao Chih Liao <perryliaohc@gmail.com>
2018-01-13 23:33:16 -0800

    removed console testing

--- server/index.js
+++ server/index.js
@@ -92,6 +92,7 @@ for(i=0; i < transaction.length; i++ ){
 }
 
 function graphTrans (data, item, startDate, endDate, lat, lon, radius){
+
   for(i=0; i < data.length; i++ ){
     if(data[i].item == item){
       if (data[i].date.getTime() > startDate.getTime() &&
@@ -149,23 +150,17 @@ function isInsideBox(latCenter, lonCenter, radius, latTest, lonTest){
 
  function getDistanceFromLatLonInM(lat1, lon1, lat2, lon2) {
    var erad = 6371000; // Radius of the earth in m
-   console.log('lon1=' + lon1);
-   console.log('lon2=' + lon2);
    var dLat = deg2rad(lat2-lat1);  // deg2rad below
-   console.log('dLat=' + dLat);
    var dLon = deg2rad(lon2-lon1);
 
-   console.log('dLon=' + dLon);
    var a =
      Math.sin(dLat/2) * Math.sin(dLat/2) +
      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
      Math.sin(dLon/2) * Math.sin(dLon/2)
      ;
-     console.log('a=' + a);
    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-console.log('c=' + c);
    var d = erad * c; // Distance in km
- console.log('d=' + d);
+
    return d;
 
  }
