2144d54be03de4c61bbe31a47593a5e0806f538a
Hao Chih Liao <perryliaohc@gmail.com>
2018-01-13 17:53:39 -0800

    Random data Generator
    also fixed usertransactions method

--- server/index.js
+++ server/index.js
@@ -7,8 +7,7 @@ const [host, port] = addr.split(":");
 
 app.get("/test", (req, res) => {
     //return all not-completed tasks in the database
-    transactions("bob");
-    res.send(allUserTransactions);
+    res.send(getUserTransactions("Bob"));
 });
 
 app.listen(port, host, () => {
@@ -16,33 +15,64 @@ app.listen(port, host, () => {
 });
 
 let allUserTransactions = [];
-let transaction  =[
+let userNames = ["Alice", "Bob", "Chris", "Doris"];
+let items = ["coffee", "bagel", "sandwich", "milkshake", "water", "salad"]
+let transaction  = generateTransactions(500);
 
-{
-  user: "bob",
- item: "coffee",
- price: 5.40,
- time: 730,
- latitude:  24.909,
- longitude:  24.909,
-
+function generateTransactions(num) {
+  let trans = [num];
+  for (i = 0; i < num; i++) {
+    trans[i] = {
+      user: userNames[getRandomInRange(0, 3, 0)],
+      item: items[getRandomInRange(0,5,0)],
+      price: getRandomInRange(0.5, 30, 2),
+      date: generateNewDate(),
+      lat: getRandomInRange(-180, 180, 3),
+      long: getRandomInRange(-90, 90, 3),
+    }
+  }
+  return trans;
 }
 
-];
+function generateNewDate() {
+  var year = getRandomInRange(2010, 2018, 0);
+  var month = getRandomInRange(1, 12, 0);
+  if (month == 4 || month == 6 || month == 9 || month == 11) {
+    var days = getRandomInRange(1, 30, 0);
+  } else if (month == 2) {
+    if (year % 4 == 0) {
+      var days = getRandomInRange(1,29,0);
+    } else {
+      var days = getRandomInRange(1, 28, 0);
+    }
+  } else {
+    var days = getRandomInRange(1,31,0);
+  }
+  var d = new Date(year,
+                  month,
+                  days,
+                  getRandomInRange(0,23,0), //hour
+                  getRandomInRange(0,60,0), //minute
+                  getRandomInRange(0,60,0), //second
+                  getRandomInRange(0,100,0)); //millisecond
+  return d;
+}
 
+function getRandomInRange(from, to, fixed) {
+    return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+    // .toFixed() returns string, so ' * 1' is a trick to convert to number
+}
 
-function transactions (name){
+function getUserTransactions (name){
 var count = 0;
 
 for(i=0; i < transaction.length; i++ ){
   if (transaction[i].user == name) {
-    if (count == allUserTransactions.length) {
-      allUserTransactions.length *= 2;
-    }
-     allUserTransactions[count++] = transaction[i];
+
+     allUserTransactions.push(transaction[i]);
     }
   }
 
- //allUserTransactions;
+ return allUserTransactions;
 
 };
