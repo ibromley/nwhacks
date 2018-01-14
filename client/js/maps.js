129df9e4e30bf9f5265bae2905f052db634f54d2
Iain Bromley <iaineubromley@gmail.com>
2018-01-14 02:03:58 -0800

    Added maps

--- client/js/maps.js
+++ client/js/maps.js
@@ -1,60 +1,42 @@
-// fake JSON call
-function getJSONMarkers() {
-   var objects = [];
-    url = "http://localhost:4000/test2";
-    var newobject = fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            transactions = data;
-            transactions.forEach(o => {
-                
-                let pins = {
-                    name: o.user,
-                    location: [o.lat, o.lon]
-                }
-                console.log(pins);
-                objects.push(pins);
-            });
-            return objects;
-        
-        })
-    .catch(err => alert(err.message));
-    
-    /*const markers = [
-        {
-        name:  "Rixos The Palm",
-        location: [25.1212, 55.1535]
-        },
-        {
-        name: "Shangri-La Hotel",
-        location: [25.2084, 55.2719]
-        },
-        {
-        name: "Grand Hyatt",
-        location: [25.2285, 55.3273]
-        }
-    ];
-    console.log(objects);
-    return objects;*/
-}
-
 function loadMap() {
     // Initialize Google Maps
     const mapOptions = {
-        center:new google.maps.LatLng(25.2048, 55.2708),
-        zoom: 11
+        center:new google.maps.LatLng(48.7949, -122.6976),
+        zoom: 7
     }
     const map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
     // Load JSON Data
-    const hotelMarkers = getJSONMarkers();
+    let hotelMarkers = getJSONMarkers();
 
-    // Initialize Google Markers
-    for(hotel of hotelMarkers) {
-        let marker = new google.maps.Marker({
-        map: map,
-        position: new google.maps.LatLng(hotel.location[0], hotel.location[1]),
-        title: hotel.name
+
+    url = "http://localhost:4000/test2";
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            transactions = data;
+            transactions.forEach(o => {
+                let marker = new google.maps.Marker({
+                    map: map,
+                    position: new google.maps.LatLng(o.lat, o.lon),
+                    title: o.user
+                    })
+            });
+
+        
         })
-    }
+    .catch(err => alert(err.message));
+
+    var cityCircle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: map,
+        center: {lat: 49.25, lng: -123.1},
+        radius: Math.sqrt(1000000) * 100
+      });
+
+    // Initialize Googl Markers
 }
