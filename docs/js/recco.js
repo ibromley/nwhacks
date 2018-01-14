255fa84ee2cc3013aab95eb23f303f3c68472251
Henry <billhung56@gmail.com>
2018-01-14 11:50:57 -0800

    ignore

--- docs/js/recco.js
+++ docs/js/recco.js
@@ -1,5 +1,3 @@
-
-
 var suggestion = new Vue({
     el: '#recco_form',
     data: {
@@ -9,21 +7,21 @@ var suggestion = new Vue({
        lon: 0
     },
     methods: {
-        getData: function(url) {
-            var url = `https://nwhacks-transactions.herokuapp.com/testrecco?name=${this.name}&lat=${this.lat}&lon=${this.lon}`;
-            return fetch(url)
-                .then(response => response.json())
-                .then(data => this.transactions = data)
-                .catch(err => alert(err.message));
-        },
-        updatevalidity: function() {
-            
-        }
     },
 });
 
 var cards = new Vue({
     el: "#item-cards",
-    data: {},
-    methods: {}
+    data: {
+      reccoList: []
+    },
+    methods: {
+      updateReccoList: function() {
+          var url = `http://localhost:4000/testrecco?name=${this.name}&lat=${this.lat}&lon=${this.lon}`;
+          return fetch(url)
+              .then(response => response.json())
+              .then(data => this.reccoList = data)
+              .catch(err => alert(err.message));
+      },
+    },
 });
