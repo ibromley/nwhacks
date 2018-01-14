597df44cacc3360e25f9eef0de9a70dfa85ed6d4
Henry <billhung56@gmail.com>
2018-01-14 09:20:01 -0800

    1. complete graph.html 2. get rid of topbar 3. change the icon for graph

---
+++ docs/js/graph.js
@@ -0,0 +1,72 @@
+var hours = []; //x-axis label
+for (let i = 0; i < 24; i++) hours.push(i);
+
+var freq = [];
+
+var itemType = "water";
+const base_url = "http://localhost:4000/testfreq/?item=";
+
+function getFrequency(url) {
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => freq = data.map((cntObj) => { return cntObj['count']; }))
+        .catch(err => alert(err.message));
+}
+
+function drawChart(freq) {
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels: hours,
+          datasets: [{
+              label: itemType,
+              data: freq,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
+  return myChart;
+}
+
+var myChart;
+getFrequency(base_url + itemType)
+  .then(() => {
+    myChart = drawChart(freq);
+    //console.log(freq);
+  })
+  .catch(err => alert(err.message));
+  
+var selectTypes = new Vue({
+  el: "#select_types",
+  data: {
+  },
+  methods: {
+    updateType() {
+      console.log(this.$el.value);
+      let newType = this.$el.value;
+      getFrequency(base_url + newType)
+        .then(() => {
+          console.log(freq);
+          myChart.data.datasets[0].data = freq;
+          myChart.update();
+        })
+        .catch(err => alert(err.message));
+    },
+  },
+});
