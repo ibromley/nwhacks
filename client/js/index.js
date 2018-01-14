287be952b0730c0aa678fbb708794fd7eba144e2
Iain Bromley <iaineubromley@gmail.com>
2018-01-13 19:31:39 -0800

    Staring with Vue

--- client/js/index.js
+++ client/js/index.js
@@ -1,19 +1,33 @@
 
+new Vue({
+    el: '#item-cards',
+    data: {
+       name: 'Hello Vue.js!'
+    },
+    methods: {
+        getData: function() {
+        
+            var url = "http://localhost:4000/all"
+        
+            var objects = [];
+            fetch(url)
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(data) {
+                    console.log(data);
+                    objects = data;
+                })
+                .catch(function(err) {
+                    console.error(err);
+                    alert(err.message);
+            });
+        
+        }
+    },
+    beforeMount(){
+        this.getData()
+     },
 
-var url = "http://localhost:4000/all"
 
-var objects = [];
-
-
-fetch(url)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        objects = data;
-    })
-    .catch(function(err) {
-        console.error(err);
-        alert(err.message);
 });
